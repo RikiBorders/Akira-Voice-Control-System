b9ff63bfa6015ad78179dc4cfdd8921622f57dfe
RikiBorders <ninjab02@hotmail.com>
2022-02-11 13:42:13 -0500

    Documentation update

--- prototype_v1.py
+++ prototype_v1.py
@@ -11,29 +11,29 @@ source = sr.Microphone()
 
 def callback(rc, audio):
     print('callback run')
-
     # key-words to initiate command listening
     keywords = [('hey akira', 1), ('akira', 1)] 
+    
     try:
         speech_as_text = rc.recognize_sphinx(audio, keyword_entries=keywords)
 
         # Look for your "hey akira" keyword in speech_as_text
         if "akira" in speech_as_text or "hey akira":
-            print('identified')
             recognize_main()
 
     except sr.UnknownValueError:
-        print("Oops! Didn't catch that")
+        print("Speech unrecognizable")
 
 
 def recognize_main():
     # Begin listening for accompanying command
     print("Activated. Recognizing main command...")
-    audio_data = rc.listen(source)
+    audio_data = rc.listen(source, phrase_time_limit=5)
 
 
 def mainloop():
-    print('main run')
+
+    # Create background thread for command listening
     rc.listen_in_background(source, callback, phrase_time_limit=3)
     time.sleep(10000) # Time to listen
 
