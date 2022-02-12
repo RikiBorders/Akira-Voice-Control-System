d6127064ceb3a4b5f11d4fc9ca03898cee87f741
RikiBorders <ninjab02@hotmail.com>
2022-02-12 14:14:24 -0500

    Added documentation to web search function

--- commands.py
+++ commands.py
@@ -4,3 +4,19 @@ executable commands. This file does not include
 command parsing, speech recognition, or anything else
 related directly to the main voice control system. 
 '''
+
+import webbrowser
+
+
+
+def web_search(terms):
+    '''
+    PARAMS:
+        terms:list: list of terms the user wants to search for
+    RETURNS: 
+        None
+    DESC: Create a new tab in the user's browser
+    and create a google search using the terms the user
+    specified.
+    '''
+    print('run')
