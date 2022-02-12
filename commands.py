85ca2c6c30e6e46f114e25e2ad8bc612c52e6d19
RikiBorders <ninjab02@hotmail.com>
2022-02-12 13:54:08 -0500

    Added commands file

---
+++ commands.py
@@ -0,0 +1,6 @@
+'''
+This file holds all of Akira's 
+executable commands. This file does not include 
+command parsing, speech recognition, or anything else
+related directly to the main voice control system. 
+'''
