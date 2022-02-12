b5e8362fd41081b0fb39236471c13cf7e45d95d3
RikiBorders <ninjab02@hotmail.com>
2022-02-12 15:00:13 -0500

    Added search functionality and command recognition

--- commands.py
+++ commands.py
@@ -4,7 +4,6 @@ executable commands. This file does not include
 command parsing, speech recognition, or anything else
 related directly to the main voice control system. 
 '''
-
 import webbrowser
 
 
@@ -12,11 +11,19 @@ import webbrowser
 def web_search(terms):
     '''
     PARAMS:
-        terms:list: list of terms the user wants to search for
+        terms:string: string of terms the user wants to search for (split by spaces)
     RETURNS: 
         None
     DESC: Create a new tab in the user's browser
     and create a google search using the terms the user
     specified.
     '''
-    print('run')
+    terms = terms.split(' ')
+    query = ''
+    for i in range(len(terms)):
+        query += terms[i]
+        if i != len(terms)-1:
+            query += '+'
+
+    url = f'https://www.google.com.tr/search?q={query}'
+    webbrowser.open_new_tab(url)
--- prototype_v1.py
+++ prototype_v1.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 from pocketsphinx import LiveSpeech
 import re
 import time
-
+from commands import *
 
 # Initialize microphone and recognizer
 rc = sr.Recognizer()
@@ -10,7 +10,6 @@ source = sr.Microphone()
 
 
 def callback(rc, audio):
-    print('callback run')
     # key-words to initiate command listening
     keywords = [('hey akira', 1), ('akira', 1)] 
     
@@ -25,14 +24,52 @@ def callback(rc, audio):
         print("Speech unrecognizable")
 
 
+
 def recognize_main():
     # Begin listening for accompanying command
     print("Activated. Recognizing main command...")
     audio_data = rc.listen(source, phrase_time_limit=5)
+    command = rc.recognize_google(audio_data).lower()
+
+    # Identify command type
+    c_type, terms = process_command(command)
+
+    if c_type == 'web_search':
+        web_search(terms)
+    else:
+        print('unknown')
+    
+
+def process_command(command):
+    '''
+    PARAMS:
+        command:str: full spoken command issued to Akira
+    RETURNS:
+        result:tuple: Contains the following data:
+            c_type:string: command type
+            clipped:string: full command with identifying keyword stub removed
+    DESC: Process command type
+    ''' 
+    websearch_kwds = set(['look up', 'google', 'search'])
+
+    result = (None, None)
+    parsed = command.split(' ')
+
+    if parsed[0] in websearch_kwds or parsed[0]+' '+parsed[1] in websearch_kwds:
+        c_type = 'web_search'
+
+        if parsed[0] in websearch_kwds:
+            parsed.pop(0)
+        else:
+            parsed = parsed[2:]
+
+        result = (c_type, ' '.join(parsed))
+
+
+    return result
 
 
 def mainloop():
-
     # Create background thread for command listening
     rc.listen_in_background(source, callback, phrase_time_limit=3)
     time.sleep(10000) # Time to listen
