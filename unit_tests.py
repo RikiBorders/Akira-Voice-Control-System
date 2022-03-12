1b0e7dd8ee5d4738b748b90c95eef6f66001b6d1
RikiBorders <ninjab02@hotmail.com>
2022-03-12 14:11:14 -0500

    Added additional test cases for mute function

--- unit_tests.py
+++ unit_tests.py
@@ -6,8 +6,6 @@ from commands import *
 import traceback
 from termcolor import colored # Color terminal text (makes tests look cool and easier to read)
 
-############### WINDOWS TESTS ###############
-
 
 class TestCase():
     ''' 
@@ -20,7 +18,8 @@ class TestCase():
         PARAMS:    
             parameters:list: list of function parameters in order
             function:function: function to test
-            message:string: Description of the test case 
+            desc:string: Description of the test case 
+            expected_output:___: Expected output for the function. parameter type varies case-to-case
         RETURNS: None
         Desc: TestCase class initializer.
         '''
@@ -67,16 +66,33 @@ class TestCase():
             return 0
 
 
+############### WINDOWS TESTS ###############
 
 def run_tests():
     print('Running tests...\n')
 
-    # Mute tests
-    case1 = TestCase([True, None], toggle_mute, 'Mute All Test', 'Mute Master Volume (mutes everything)')
-    case2 = TestCase([False, None], toggle_mute, 'Unmute All Test', 'Unmute Master Volume (unmutes everything)')
+    # Mute tests for master volume
+    case1 = TestCase([True, None], toggle_mute, 'Mute All', 'Mute Master Volume (mutes everything)')
+    case2 = TestCase([False, None], toggle_mute, 'Unmute All', 'Unmute Master Volume (unmutes everything)')
 
-    case1.run()
+    # Mute tests for specific applications
+    case3 = TestCase([True, 'chrome'], toggle_mute, 'Mute Google Chrome', 'Mute Google Chrome Volume')
+    case4 = TestCase([False, 'chrome'], toggle_mute, 'Unmute Google Chrome', 'Unmute Google Chrome Volume')
+    case5 = TestCase([True, 'chrome.exe'], toggle_mute, 'Errenous Mute call', 'Try to mute chrome with errenous call (will generate the app name "chrome.exe.exe")')
+    case6 = TestCase([False, 'chrome.exe'], toggle_mute, 'Errenous Unmute call', 'Try to Unmute chrome with errenous call (will generate app name "chrome.exe.exe"')
+
+    # Mute apps that don't exist
+    case7 = TestCase([True, 'jvodsvjds jjen q'], toggle_mute, 'Mute non-existent', 'Try to mute nonexistent application')
+    case8 = TestCase([False, 'jvodsvjds jjen q'], toggle_mute, 'Unmute non-existent', 'Try to Unmute nonexistent application')
+
+
+    # Run tests
+    case1.run() 
     case2.run()
+    case3.run()
+    case4.run()
+    case5.run()
+    case6.run()
 
 
 if __name__ == "__main__":
